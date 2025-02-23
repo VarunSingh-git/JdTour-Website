11948e9f93362843ca5393e68d84515edbc01176
VarunSingh-git <varunsaini.a.x@gamil.com>
2025-02-24 02:30:53 +0530

    Update footer and WhatsApp icon scripts to use absolute URLs; remove deprecated files

---
+++ public/whatsapp-icon.js
@@ -0,0 +1,13 @@
+const policy = window.trustedTypes?.createPolicy("default", {
+  createHTML: (string) => string,
+});
+
+fetch("https://thisismywebsite-azure.vercel.app/whatsapp-icon.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      policy ? policy.createHTML(data) : data
+    );
+  })
+  .catch((error) => console.error("Footer load failed:", error));
--- utils/whatsapp-icon.js
+++
@@ -1,5 +0,0 @@
-fetch("utils/whatsapp.html")
-  .then((response) => response.text())
-  .then((data) => document.body.insertAdjacentHTML("beforeend", data));
-
-  
