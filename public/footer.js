da8fa2ee69e37bbb04cb2a8c7828f0faaf559590
VarunSingh-git <varunsaini.a.x@gamil.com>
2025-02-24 02:24:47 +0530

    Refactor footer.js to use absolute URL for footer HTML and reintroduce trusted types policy for enhanced security

--- public/footer.js
+++ public/footer.js
@@ -1,17 +1,13 @@
-// const policy = window.trustedTypes?.createPolicy('default', {
-//   createHTML: (string) => string
-// });
-
-// fetch("/utils/footer.html")
-//   .then((response) => response.text())
-//   .then((data) => {
-//     document.body.insertAdjacentHTML(
-//       "beforeend",
-//       policy ? policy.createHTML(data) : data
-//     );
-//   })
-//   .catch((error) => console.error("Footer load failed:", error));
+const policy = window.trustedTypes?.createPolicy("default", {
+  createHTML: (string) => string,
+});
 
 fetch("https://thisismywebsite-azure.vercel.app/footer.html")
   .then((response) => response.text())
-  .then((data) => document.body.insertAdjacentHTML("beforeend", data));
+  .then((data) => {
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      policy ? policy.createHTML(data) : data
+    );
+  })
+  .catch((error) => console.error("Footer load failed:", error));
