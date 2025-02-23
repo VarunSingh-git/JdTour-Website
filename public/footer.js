a1e6abbf6f906c5c242f48281210290a930bb74a
VarunSingh-git <varunsaini.a.x@gamil.com>
2025-02-24 02:11:59 +0530

    Update footer script source in index.html to use absolute URL; refactor footer.js to fetch HTML from new location

--- public/footer.js
+++ public/footer.js
@@ -1,13 +1,17 @@
-const policy = window.trustedTypes?.createPolicy('default', {
-  createHTML: (string) => string
-});
+// const policy = window.trustedTypes?.createPolicy('default', {
+//   createHTML: (string) => string
+// });
 
-fetch("/utils/footer.html")
+// fetch("/utils/footer.html")
+//   .then((response) => response.text())
+//   .then((data) => {
+//     document.body.insertAdjacentHTML(
+//       "beforeend",
+//       policy ? policy.createHTML(data) : data
+//     );
+//   })
+//   .catch((error) => console.error("Footer load failed:", error));
+
+fetch("https://thisismywebsite-azure.vercel.app/footer.html")
   .then((response) => response.text())
-  .then((data) => {
-    document.body.insertAdjacentHTML(
-      "beforeend",
-      policy ? policy.createHTML(data) : data
-    );
-  })
-  .catch((error) => console.error("Footer load failed:", error));
+  .then((data) => document.body.insertAdjacentHTML("beforeend", data));
