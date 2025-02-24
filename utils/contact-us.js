43965b6af42ac5d7f153ca5814c55d30f1d01e06
VarunSingh-git <varunsaini.a.x@gamil.com>
2025-02-25 02:39:05 +0530

    Update script versions in HTML files and remove unused contact-us.js

--- utils/contact-us.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-  document.querySelector(".nav-links").classList.toggle("active");
-}
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".menu-toggle").addEventListener("click", toggleMenu);
-});
-
