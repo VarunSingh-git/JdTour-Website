81c88c1ecab48fb2dd91757e0817964bf7234439
VarunSingh-git <varunsaini.a.x@gamil.com>
2025-02-25 01:33:06 +0530

    ``` No code changes detected; skipping commit. ```

--- public/footer.js
+++ public/footer.js
@@ -1,3 +1,6 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/dompurify/3.0.6/purify.min.js"></script>
+
+
 fetch("https://thisismywebsite-azure.vercel.app/footer.html?v=1.2")
   .then((response) => response.text())
   .then((data) => {
