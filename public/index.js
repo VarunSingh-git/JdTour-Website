bf0cb07d02858ef31173b37720d9ac66bc0f7476
VarunSingh-git <varunsaini.a.x@gamil.com>
2025-03-09 01:12:33 +0530

    Update CSS versioning across multiple files and adjust navbar padding for consistency

--- public/index.js
+++ public/index.js
@@ -31,18 +31,6 @@ document.addEventListener("click", function (event) {
   }
 });
 
-// function sendEmail() {
-//   let email = "jdtravel07@gmail.com";
-//   let subject =
-//     "Namaste, Welcome to Jagdamba Tour and Travels, Ride Enjoy & Repeat ❤️";
-//   let body =
-//     "We will soon answer your E-mail. \nWarm Regards, JD Tour & Travels..";
-//   let mailtoLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${encodeURIComponent(
-//     subject
-//   )}&body=${encodeURIComponent(body)}`;
-
-//   window.open(mailtoLink, "_blank");
-// }
 function sendEmail() {
   let email = "jdtravel07@gmail.com";
   let subject =
